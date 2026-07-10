bb08419f96f124bdf6ac6be35fcb6a4fc054e3ea
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-07-10 02:23:38 -0500

    Updated 1/10/2024

--- divestitures.py
+++ divestitures.py
@@ -6,12 +6,14 @@ Created on Mon Jul 6 2026
 
 """
 
+import pandas as pd
+
 from finance.logging import Logging
 from options.spreads import Spread
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = []
+__all__ = ["DivestitureCalculator", "Divestiture"]
 __copyright__ = "Copyright 2026, Jack Kirby Cook"
 __license__ = "MIT License"
 
@@ -21,7 +23,8 @@ class Divestiture(Spread):
 
 
 class DivestitureCalculator(Logging):
-    pass
+    def __call__(self, options, **kwargs):
+        assert isinstance(options, pd.DataFrame)
 
 
 
--- spreads.py
+++ spreads.py
@@ -6,163 +6,80 @@ Created on Sat May 16 2026
 
 """
 
-import numpy as np
 import pandas as pd
-from typing import Optional
 from types import SimpleNamespace
-from functools import total_ordering
-from dataclasses import dataclass, fields
 
-from finance.enumerations import Spread
+from finance.enumerations import Strategy
 from finance.osi import OSI
 from support.custom import DateRange
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ["Spread", "Metrics"]
+__all__ = ["Spread"]
 __copyright__ = "Copyright 2026, Jack Kirby Cook"
 __license__ = "MIT License"
 
 
-@total_ordering
-@dataclass(frozen=True)
-class Profit:
-    valuation: float; market: float
-
-    def __lt__(self, other): return float(self) < float(other)
-    def __float__(self): return self.valuation - self.market
-
-@total_ordering
-@dataclass(frozen=True)
-class Quality:
-    zscore: float; gap: float; profit: Profit
-
-    def __lt__(self, other): return float(self) < float(other)
-    def __float__(self): return np.abs(self.zscore) * float(self.profit) / max(self.gap, 1e-12)
-
-@total_ordering
-@dataclass(frozen=True)
-class Risk:
-    gamma: float; theta: float; vega: float
-
-    def __lt__(self, other): return float(self) < float(other)
-    def __float__(self): return abs(self.gamma) + abs(self.vega) + max(0.0, -self.theta)
-
-@total_ordering
-@dataclass(frozen=True)
-class Score:
-    profit: Profit; quality: Quality; risk: Risk
-
-    def __lt__(self, other): return float(self) < float(other)
-    def __float__(self):
-        zscore = abs(self.quality.zscore)
-        edge = max(float(self.profit), 1e-12)
-        gap = float(self.quality.gap) / edge
-        theta = self.risk.theta / edge
-        gamma = abs(self.risk.gamma) / edge
-        vega = abs(self.risk.vega) / edge
-        return zscore - 1.5 * gap + 0.5 * theta - 0.5 * gamma - 0.25 * vega
-
-
-@dataclass(frozen=True)
-class Ratios:
-    gamma: Optional[float] = None; theta: Optional[float] = None; vega: Optional[float] = None
-    gap: Optional[float] = None
-
-@dataclass(frozen=True)
-class Metrics:
-    ratios: Ratios; zscore: float; profit: float
-
-    @classmethod
-    def create(cls, /, ratios, zscore, profit):
-        assert isinstance(ratios, dict) and isinstance(zscore, float) and isinstance(profit, float)
-        ratios = {field.name: ratios.get(field.name, None) for field in fields(Ratios)}
-        ratios = Ratios(**ratios)
-        return cls(ratios, zscore, profit)
-
-
 class Spread(object):
-    def __init__(self, legs): self.legs = legs
-    def __new__(cls, legs):
-        assert isinstance(legs, pd.DataFrame)
-        assert len(legs["ticker"].unique()) == 1
-        assert len(legs["type"].unique()) == 1
-        instance = super().__new__(cls)
-        return instance
+    def __init__(self, strategy, securities):
+        assert isinstance(securities, pd.DataFrame)
+        assert len(securities["ticker"].unique()) == 1
+        assert strategy in list(Strategy)
+        self.__expires = DateRange.create(securities["expire"].to_list())
+        self.__ticker = securities["ticker"].unique()[0]
+        self.__securities = securities
+        self.__strategy = strategy
+
+    def __iter__(self):
+        for osi, position, quantity in zip(self.osi, self.position, self.quantity):
+            yield SimpleNamespace(osi=osi, position=position, quantity=quantity)
 
     @property
-    def signature(self): return tuple((str(record.osi), int(record.position), int(record.quantity)) for record in self.records)
-    @property
-    def records(self):
-        function = lambda value: (str(value[0]), int(value[1]), int(value[2]))
-        keys, values = ["osi", "position", "quantity"], list(zip(self.osi, self.position, self.quantity))
-        records = [dict(zip(keys, value)) for value in sorted(values, key=function)]
-        return [SimpleNamespace(**record) for record in records]
+    def signature(self): return tuple((str(record.osi), int(record.position), int(record.quantity)) for record in self)
 
     @property
-    def osi(self): return self.legs[["ticker", "expire", "option", "strike"]].apply(OSI, axis=1)
+    def osi(self): return self.securities[["ticker", "expire", "option", "strike"]].apply(OSI, axis=1)
     @property
-    def cost(self): return (self.legs["median"] * self.position.map(int) * self.quantity).sum()
+    def cost(self): return (self.securities["median"] * self.position.map(int) * self.quantity).sum()
 
     @property
-    def score(self): return Score(self.profit, self.quality, self.risk)
+    def gamma(self): return (self.securities["gamma"] * self.position.map(int) * self.quantity).sum()
     @property
-    def profit(self): return Profit(self.valuation, self.market)
+    def theta(self): return (self.securities["theta"] * self.position.map(int) * self.quantity).sum()
     @property
-    def quality(self): return Quality(self.zscore, self.gap, self.profit)
-    @property
-    def risk(self): return Risk(self.gamma, self.theta, self.vega)
+    def vega(self): return (self.securities["vega"] * self.position.map(int) * self.quantity).sum()
 
     @property
-    def gamma(self): return (self.legs["gamma"] * self.position.map(int) * self.quantity).sum()
+    def valuation(self): return (self.securities["value"] * self.position.map(int) * self.quantity).sum()
     @property
-    def theta(self): return (self.legs["theta"] * self.position.map(int) * self.quantity).sum()
+    def market(self): return (self.securities["median"] * self.position.map(int) * self.quantity).sum()
     @property
-    def vega(self): return (self.legs["vega"] * self.position.map(int) * self.quantity).sum()
+    def gap(self): return (self.securities["gap"] * self.quantity).sum()
 
     @property
-    def valuation(self): return (self.legs["value"] * self.position.map(int) * self.quantity).sum()
+    def tightness(self): return self.securities["tightness"].max()
     @property
-    def market(self): return (self.legs["median"] * self.position.map(int) * self.quantity).sum()
+    def moneyness(self): return self.securities["moneyness"].max()
     @property
-    def gap(self): return (self.legs["gap"] * self.quantity).sum()
+    def activity(self): return self.securities["activity"].min()
 
     @property
-    def ratios(self):
-        gamma = self.gamma / max(float(self.profit), 1e-12)
-        theta = self.theta / max(float(self.profit), 1e-12)
-        vega = self.vega / max(float(self.profit), 1e-12)
-        gap = self.gap / max(float(self.profit), 1e-12)
-        return Ratios(gamma=gamma, theta=theta, vega=vega, gap=gap)
+    def position(self): return self.securities["position"]
+    @property
+    def quantity(self): return self.securities["quantity"]
 
     @property
-    def zscore(self):
-        if self.type is Spread.FLY:
-            left, center, right = self.legs["zscore"].to_numpy()
-            return center - (left + right) / 2
-        elif self.type is Spread.CALENDAR:
-            near, far = self.legs["zscore"].to_numpy()
-            return far - near
-        else: raise ValueError(self.type)
+    def securities(self): return self.securities
+    @property
+    def strategy(self): return self.strategy
+    @property
+    def expires(self): return self.__expires
+    @property
+    def ticker(self): return self.__ticker
 
-    @property
-    def tightness(self): return self.legs["tightness"].max()
-    @property
-    def moneyness(self): return self.legs["moneyness"].max()
-    @property
-    def activity(self): return self.legs["activity"].min()
 
-    @property
-    def position(self): return self.legs["position"]
-    @property
-    def quantity(self): return self.legs["quantity"]
 
-    @property
-    def ticker(self): return str(list(self.legs["ticker"].unique()[0]))
-    @property
-    def expires(self): return DateRange.create(self.legs["expire"].to_list())
-    @property
-    def type(self): return list(self.legs["type"].unique())[0]
+
 
 
 
